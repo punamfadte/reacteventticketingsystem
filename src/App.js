0bbee0185c2ca635e35bfe21d0997d0f47b146b3
webfillsgoa <webfillsgoa@gmail.com>
2021-08-28 11:56:58 +0530

    Initial - React app and page components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import Header from './components/header/Header';
+import Dashboard from './components/dashboard/Dashboard';
+import Events from './components/events/Events';
+import Orders from './components/orders/Orders';
+import Customers from './components/customers/Customers';
+import Reviews from './components/reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/events">
+          <Events></Events>
+        </Route>
+        <Route path="/orders">
+          <Orders></Orders>
+        </Route>
+        <Route path="/customers">
+          <Customers></Customers>
+        </Route>
+        <Route path="/reviews">
+          <Reviews></Reviews>
+        </Route>
+      </Switch>
     </div>
   );
 }
 
-export default App;
+export default withRouter((App));
---
+++ src/components/reviews/Reviews.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export default function Reviews() {
+    return (
+        <div>Reviews</div>
+    );
+}
